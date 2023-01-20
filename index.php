90be6a4f37e9977bfe9511aea11f50ffacca2d8d
victorien <deadvicmangame@gmail.com>
2023-01-20 10:54:06 +0100

    constantes et constantes magiques exo 165

--- index.php
+++ index.php
@@ -9,9 +9,21 @@
  *    longueur de la chaîne de la troisième constante ( attention aux priorités )
  */
 // TODO Votre code ici.
+const MA_CONSTANTE = '12';
+const MA_CONSTANTE2 = '2';
+const MA_CONSTANTE3 = "C'est cool PHP <br>";
 
+echo MA_CONSTANTE * MA_CONSTANTE2;
+$result = (MA_CONSTANTE + MA_C0NSTANTE2) * strlen(MA_CONSTANTE3) . "<br>";
+echo $result;
 
 /**
  * 6. Utilisez la bonne constante magique et éventuellement la bonne fonction pour afficher un maximum d'informations sur la page actuelle4
  */
 // TODO Votre code ici.
+echo "Répértoire du fichier:" . __FILE__ . "<br>";
+echo "Répértoire :" . __DIR__  . "<br>";
+echo "Ligne numéro :" . __LINE__ . "<br>";
+echo "Ligne numéro :" . __METHOD__  . "<br>";
+echo "Ligne numéro :" . __NAMESPACE__  . "<br>";
+echo "Ligne numéro :" . __TRAIT__  . "<br>";
